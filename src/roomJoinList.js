d34f5bbd43dd7d82c6125a1999d6f9c93c22f5d0
manhasscar <cpdlxkr23@gmail.com>
2023-02-27 18:09:39 +0900

    12

---
+++ src/roomJoinList.js
@@ -0,0 +1,29 @@
+import { API_END_POINT } from "./API.js";
+const $rooms = document.querySelector('.roomlist'); 
+const userUid = sessionStorage.getItem('UID');
+axios.get(`${API_END_POINT}/user/${userUid}/groups/joined`)
+.then((response) => {
+  const rooms = response.data;
+  const roomElements = rooms.map((room) => {
+    const roomElement = document.createElement('div');
+    roomElement.className = 'room';
+    roomElement.dataset.id = room.gid;
+    roomElement.innerHTML = `
+      <h3>${room.group_title}</h3>
+      <p>${room.group_detail}</p>
+      <p>by ${room.master_uid}</p>
+      <p>참가자 수 : ${room.head_count}</p>
+    
+    `;
+    roomElement.addEventListener('click', () =>{
+      window.location.href = `room.html?id=${room.gid}`;
+    });
+    return roomElement;
+  })
+
+  $rooms.append(...roomElements)
+
+})
+.catch((err) => {
+    console.error(err.message)
+  })
