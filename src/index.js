cd877dde9394c761bc00bb2900482d6d49e0597b
Third rate coder <62248486+manhasscar@users.noreply.github.com>
2023-03-24 17:43:33 +0900

    Kyoungseop (#15)
    * post
    
    * 코드최적화
    
    * 1212
    
    * 2-21
    
    * 방추가
    
    * 출석코드 생성중
    
    * atten_code
    
    * testlogin
    
    * 12
    
    * Css적용 및 출석체크 디비삽입
    
    * cssUpdate
    
    * feedback_update
    
    * floating btn
    
    * 1
    
    * loginpage
    
    * loginPage
    
    * loginPage
    
    * loginpage
    
    * login
    
    * 32
    
    * main page
    
    * 출석체크 기능 구현
    
    ---------
    
    Co-authored-by: manhasscar <cnu@CNU05ui-iMac.local>

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const userUid = sessionStorage.getItem("UID");
+const $navigation = document.querySelector(".navigation");
+const navigationUserDiv = document.createElement("div");
+navigationUserDiv.className = "accountInfo";
+const $accountMenu = document.createElement("a");
+$accountMenu.href = "userInfo.html";
+$accountMenu.textContent = "현재 로그인한 유저" + userUid;
+const $logOutBtn = document.createElement("a");
+$logOutBtn.href = "javascript:logOut()";
+$logOutBtn.textContent = "로그아웃";
+navigationUserDiv.append($accountMenu, $logOutBtn);
+$navigation.appendChild(navigationUserDiv);
--- src/roomCreate.js
+++ src/roomCreate.js
@@ -1,76 +1,58 @@
-
 import { API_END_POINT } from "./API.js";
-const $groupCreate = document.querySelector('.room-create')
+const $groupCreate = document.querySelector(".roomCreate");
 
+const groupCreateElements = document.createElement("form");
 
-const groupCreateElements = document.createElement('form');
-
-groupCreateElements.name = 'groupForm';
-groupCreateElements.id = 'groupForm';
+groupCreateElements.name = "groupForm";
+groupCreateElements.id = "groupForm";
 
 groupCreateElements.innerHTML = `
-    <p>방 이름</p>
-    <input type="text" id="groupName" class="input-line">
-    <p>방 설명</p>
-    <input type="text" id="groupDescription" class="input-line">
-    <button type="button" id="groupCreateButton">모임 개설하기</button>
+<b><font size="14" color="gray">새로운 그룹 생성하기</font></b>
+    <br><br>
+    <input type="text" placeholder="그룹 이름"id="groupName" class="input-line" style="width:200px; height:35px">
+    <br><br>
+    <input type="text" placeholder="그룹 설명"id="groupDescription" class="input-line" style="width:200px; height:35px">
+    <br><br>
+    <button type="button" id="groupCreateButton">그룹 개설하기</button>
+    <button type="button" onclick="history.back()">돌아가기</button>
 
 `;
 $groupCreate.appendChild(groupCreateElements);
 
-
 const $groupCreateButton = document.querySelector("#groupCreateButton");
 
-$groupCreateButton.addEventListener('click',() => {
-    const groupName = document.getElementById('groupName').value
-    const groupDes = document.getElementById('groupDescription').value
+$groupCreateButton.addEventListener("click", () => {
+  const groupName = document.getElementById("groupName").value;
+  const groupDes = document.getElementById("groupDescription").value;
 
-    console.log(groupName);
-    console.log(groupDes);
+  console.log(groupName);
+  console.log(groupDes);
 
-    if(groupName && groupDes){
-        postGroupCreate(groupName,groupDes);
-    }
-    else{
-        alert("정보를 전부입력해 주세요");
-    }
+  if (groupName && groupDes) {
+    postGroupCreate(groupName, groupDes);
+  } else {
+    alert("정보를 전부입력해 주세요");
+  }
+});
 
-})
-
-
-
-function postGroupCreate(groupName,groupDes){
-    const userUid = Number(sessionStorage.getItem('UID'));
-    let nowTime = new Date();
-    console.log(nowTime)
-    axios.post(`${API_END_POINT}/group`, 
-        {
-            
-            'uid' : userUid,
-            'group_title' : groupName,
-            'group_detail' : groupDes,
-            
-            
-        }
-    )
+function postGroupCreate(groupName, groupDes) {
+  const userUid = Number(sessionStorage.getItem("UID"));
+  let nowTime = new Date();
+  console.log(nowTime);
+  axios
+    .post(`${API_END_POINT}/group`, {
+      uid: userUid,
+      group_title: groupName,
+      group_detail: groupDes,
+    })
     .then(function (response) {
-        console.log(response);
-        const inviteCode = response.data;
-        alert(`그룹 개설이 완료되었습니다. 초대코드:${inviteCode}`)
-        
-       
-        location.href='index.html'
-        
+      console.log(response);
+      const inviteCode = response.data;
+      alert(`그룹 개설이 완료되었습니다. 초대코드:${inviteCode}`);
+
+      location.href = "roomCreateList.html";
     })
     .catch(function (error) {
-        console.log(error);
+      console.log(error);
     });
-    
-
 }
-
-
-
-
-
-
